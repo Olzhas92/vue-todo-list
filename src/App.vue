e6ee73c7ff1d96a32200e7a49e4482b40d9dfdd6
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-11-06 21:06:13 +0600

    adding font and global styles

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,18 @@
 import { RouterView } from "vue-router";
 </script>
 
-<style></style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Roboto:wght@300;400;500;700&display=swap");
+
+* {
+  font-family: "Roboto", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
