93e9e509773bd7bd38c02124238b7f3f44751a1a
Olzhas92 <96730962+Olzhas92@users.noreply.github.com>
2023-11-07 15:54:01 +0600

    Will add styles to navigation bar and add a form to create new todos  (#1)
    * adding a logo icon
    
    * adding styles and routing to navigation
    
    * adding a form to create a new todo

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,83 @@
+<template>
+  <header>
+    <nav class="header">
+      <RouterLink to="/" class="header__branding">
+        <img src="../assets/to-do-list.png" />
+        <h1>ToDo List</h1>
+      </RouterLink>
+      <ul class="header__nav-routes">
+        <li class="header__nav-routes--home">
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li class="header__nav-routes--about">
+          <RouterLink to="/about">About</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: lightcoral;
+
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 16px 110px;
+
+    &__branding {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 16px;
+      width: 30%;
+      text-decoration: none;
+      color: #000000;
+      img {
+        width: 24px;
+        height: auto;
+        cursor: pointer;
+      }
+
+      h1 {
+        font-size: 24px;
+        cursor: pointer;
+      }
+    }
+
+    &__nav-routes {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 20%;
+      list-style: none;
+      text-decoration: none;
+      &--home,
+      &--about {
+        min-height: 20px;
+        a {
+          text-decoration: none;
+          color: #000000;
+          font-weight: 500;
+
+          &:hover {
+            color: #ffffff;
+          }
+        }
+
+        &:hover {
+          cursor: pointer;
+          border-bottom: 1px solid blue;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="form">
+    <input type="text" placeholder="Add ToDo" />
+    <button>Create</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.form {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
+</style>
